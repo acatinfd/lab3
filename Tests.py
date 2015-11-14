37461bffb6dc0716af0f3936be349d0856be8d16
acatinfd <xiaorong@utexas.edu>
2015-11-13 23:51:11 -0600

    Test for coverage

--- Tests.py
+++ Tests.py
@@ -19,7 +19,7 @@ class MyTestCase(unittest.TestCase):
   def test_t4(self):
     r1 = Experiment.largest([3,2,3,4])
     self.assertEqual(r1, 4)
-
+"""
   def test_t5(self):
     r1 = Experiment.largest([3,-2,3,-1,4])
     self.assertEqual(r1, 4)
@@ -28,8 +28,8 @@ class MyTestCase(unittest.TestCase):
     r1 = Experiment.largest([-3,-2,-3,-1,-4])
     self.assertEqual(r1, -1)
 
-  def test_t7(self):
-    self.assertRaises(ValueError, Experiment.largest, [])
-
+  #def test_t7(self):
+    #self.assertRaises(ValueError, Experiment.largest, [])
+"""
 if __name__ == '__main__':
   unittest.main()
